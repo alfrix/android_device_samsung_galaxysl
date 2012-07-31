b835fe0b954816b03ef8aae72d049916c736e508
Alfredo <alfredomonclus@gmail.com>
2012-07-30 22:30:34 -0300

    Remove leftovers of bcm4329
    Change-Id: I48dbed3c026b0ffb632c9fedbbec03b0b79d2e7a

--- galaxysl.mk
+++ galaxysl.mk
@@ -16,7 +16,6 @@
 PRODUCT_COPY_FILES := \
 	device/samsung/galaxysl/etc/asound.conf:system/etc/asound.conf \
 	device/samsung/galaxysl/etc/gps.conf:system/etc/gps.conf \
-	device/samsung/galaxysl/etc/nvram_net.txt:system/etc/nvram_net.txt \
 	device/samsung/galaxysl/etc/gps.xml:system/vendor/etc/gps.xml \
 	device/samsung/galaxysl/etc/vold.fstab:system/etc/vold.fstab \
 	device/samsung/galaxysl/etc/media_codecs.xml:system/etc/media_codecs.xml \
